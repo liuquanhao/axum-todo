99496dc1c5d3e1020d962760c39df25c15662188
liuxu <i@liuxu.me>
2022-10-17 23:04:27 +0800

    init:database success

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+//! Run example
+//! 
+//! ```bash
+//! POSTGRESQL_URL="postgres://todouser:todopassword@127.0.0.1:5432/todos" cargo run
+//! ```
+
 use axum::{
     http::StatusCode,
     response::IntoResponse,
@@ -8,28 +14,31 @@ use axum::{
         Json,
         Extension,
         Query,
-        // FromRequest,
-        // RequestParts,
     },
-    // async_trait,
 };
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use std::{
     sync::{Arc},
     net::SocketAddr,
+    env,
 };
-use tokio_postgres::{connect, Client, NoTls};
-// use futures::{
-//     stream::futures_unordered::FuturesUnordered
-// };
+use tokio_postgres::{
+    connect,
+    Client,
+    NoTls,
+};
 
 #[tokio::main]
 async fn main() {
 
     tracing_subscriber::fmt::init();
 
-    let db_connect = connect_pg().await;
+    let pg_url = match env::var("POSTGRESQL_URL") {
+        Ok(url) => url,
+        Err(_) => panic!("Need env: POSTGRESQL_URL.")
+    };
+    let db_connect = connect_pg(&pg_url).await;
 
     let app = Router::new()
         .route("/todos/", post(create_todo).get(list_todo))
@@ -44,59 +53,73 @@ async fn main() {
 }
 
 
-async fn create_todo(Extension(client): Extension<Arc<Client>>, Json(payload): Json<CreateTodo>) -> impl IntoResponse {
+async fn create_todo(Extension(client): Extension<Arc<Client>>, Json(payload): Json<CreateTodo>) -> Result<impl IntoResponse, StatusCode> {
     let todo = Todo {
         id: Uuid::new_v4(),
         text: payload.text,
         completed: false,
     };
-    client.execute("INSERT INTO todos (id, text, completed) VALUES ($1, $2, $3)", &[&todo.id, &todo.text, &todo.completed]).await.expect("插入失败");
-    (StatusCode::CREATED, Json(todo))
+    match client.execute("INSERT INTO todos (id, text, completed) VALUES ($1, $2, $3)", &[&todo.id, &todo.text, &todo.completed]).await {
+        Ok(_) => Ok((StatusCode::CREATED, Json(todo))),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
 }
 
 async fn delete_todo(Path(id): Path<Uuid>, Extension(client): Extension<Arc<Client>>) -> impl IntoResponse {
     match client.execute("DELETE FROM todos WHERE id = $1", &[&id]).await {
         Ok(_) => StatusCode::NO_CONTENT,
-        Err(_) => StatusCode::NOT_FOUND
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
     }
 }
 
-async fn update_todo(Path(id): Path<Uuid>, Json(payload): Json<UpdateTodo>, Extension(client): Extension<Arc<Client>>) -> impl IntoResponse {
-    let row = client.query_one("SELECT * FROM todos WHERE id = $1", &[&id]).await.unwrap();
-    let mut tmp_text = String::new();
+async fn update_todo(Path(id): Path<Uuid>, Json(payload): Json<UpdateTodo>, Extension(client): Extension<Arc<Client>>) -> Result<impl IntoResponse, StatusCode> {
+    let row = match client.query_one("SELECT * FROM todos WHERE id = $1", &[&id]).await {
+        Ok(row) => row,
+        Err(_) => return Err(StatusCode::NOT_FOUND),
+    };
+    let tmp_text:String;
     if let Some(text) = payload.text {
         tmp_text = text;
     } else {
         tmp_text = row.get("text");
     }
-    let mut tmp_completed = false;
+    let tmp_completed:bool;
     if let Some(completed) = payload.completed {
         tmp_completed = completed;
     } else {
         tmp_completed = row.get("completed");
     }
-    client.query_one("UPDATE todos SET text = $1, completed = $2", &[&tmp_text, &tmp_completed]).await.unwrap();
-    StatusCode::NO_CONTENT
+    match client.execute("UPDATE todos SET text = $1, completed = $2 WHERE id = $3", &[&tmp_text, &tmp_completed, &id]).await {
+        Ok(_) => Ok(StatusCode::NO_CONTENT),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
 }
 
-async fn get_todo(Path(id): Path<Uuid>, Extension(client): Extension<Arc<Client>>) -> impl IntoResponse {
-    let row = client.query_one("SELECT * FROM todos WHERE id = $1", &[&id]).await.unwrap();
+async fn get_todo(Path(id): Path<Uuid>, Extension(client): Extension<Arc<Client>>) -> Result<impl IntoResponse, StatusCode>  {
+    let row = match client.query_one("SELECT * FROM todos WHERE id = $1", &[&id]).await {
+        Ok(row) => row,
+        Err(_) => return Err(StatusCode::NOT_FOUND)
+    };
     let todo = Todo {
         id: row.get("id"),
         text: row.get("text"),
         completed: row.get("completed"),
     };
-    (StatusCode::OK, Json(todo))
+    Ok((StatusCode::OK, Json(todo)))
 }
 
 async fn list_todo(pagination: Option<Query<Pagination>>, Extension(client): Extension<Arc<Client>>) -> impl IntoResponse {
     let Query(pagination) = pagination.unwrap_or_default();
-    let offset = (pagination.page.unwrap_or(0) * pagination.per_page.unwrap_or(10)) as u32;
-    let per_page = pagination.per_page.unwrap_or(10) as u32;
-    let stream = client.query("SELECT * FROM todos OFFSET $1 LIMIT $2", &[&offset, &per_page]).await.unwrap();
+    let offset = ((pagination.page.unwrap_or(1) - 1) * pagination.per_page.unwrap_or(10)) as i64;
+    let per_page = pagination.per_page.unwrap_or(10) as i64;
+    let stream = client.query("SELECT * FROM todos OFFSET $1 LIMIT $2", &[&offset, &per_page]).await;
+    let rows = match stream {
+        Ok(rows) => rows,
+        Err(_) => vec![],
+    };
     let mut todos = Vec::new();
-    for row in &stream {
-        let mut todo = Todo {
+    for row in &rows {
+        let todo = Todo {
             id: row.get("id"),
             text: row.get("text"),
             completed: row.get("completed"),
@@ -106,8 +129,8 @@ async fn list_todo(pagination: Option<Query<Pagination>>, Extension(client): Ext
     (StatusCode::OK, Json(todos))
 }
 
-async fn connect_pg() -> Arc<Client> {
-    let pg_url = "";
+async fn connect_pg(pg_url: &str) -> Arc<Client> {
+    // let pg_url = "postgres://todouser:todopassword@127.0.0.1:5432/todos";
     let (client, conn) = connect(pg_url, NoTls).await.expect("无法连接数据库");
     tokio::spawn(async move {
         if let Err(error) = conn.await {
@@ -116,31 +139,6 @@ async fn connect_pg() -> Arc<Client> {
     });
     Arc::new(client)
 }
-
-// pub struct DatabaseConnection(pub Arc<Client>);
-
-// #[async_trait]
-// impl<B> FromRequest<B> for DatabaseConnection
-// where
-//     B: Send,
-// {
-//     type Rejection = (StatusCode, String);
-
-//     async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
-//         let Extension(pg_connection) = Extension::<Arc<Client>>::from_request(req)
-//             .await
-//             .map_err(innernal_error)?;
-
-//         Ok(Self(pg_connection))
-//     }
-// }
-
-// fn innernal_error<E>(err: E) -> (StatusCode, String)
-// where
-//     E: std::error::Error,
-// {
-//     (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
-// }
 
 #[derive(Debug, Deserialize)]
 struct CreateTodo {
